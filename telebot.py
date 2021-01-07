7ebf8934a6203b3a3e1256e377fee27f590188d6
Monylait <48233779+Monylait@users.noreply.github.com>
2021-01-07 17:29:40 +1000

    Update telebot.py

--- telebot.py
+++ telebot.py
@@ -1,5 +1,3 @@
-#aiogram aiomongo  
-
 from aiogram import Bot, types
 from pymongo import MongoClient
 from aiogram.utils import executor
@@ -15,8 +13,8 @@ from starlette.responses import JSONResponse
 from starlette.middleware import Middleware
 
 from weebhook import set_weebhook
-from keyboard import board_1, board_3, board_5
-from functions import parser, db_list, create_inline_keyboard, create_reply_keyboard
+from keyboard import board_1, board_2, board_3
+from functions import parser, db_list, num_list, create_reply_keyboard
 from config import TOKEN, MAIN_DB, ADMIN_DB, PASSWORD
 
 client = MongoClient("localhost", 27017) 
@@ -24,13 +22,8 @@ db = client['NEW_DB']
 new_collection = db[MAIN_DB]
 adm_collection = db[ADMIN_DB]
 
-
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=MongoStorage())
-
-
-
-
 
 class States(Helper):
     mode = HelperMode.snake_case
@@ -44,52 +37,46 @@ class States(Helper):
     CHANGE = Item()
 
 async def home(request: Request):
-    if request.json()['message'][0].type ==types.CallbackQuery:
-        callback_query_handler(request)
-    elif request.json()['message'][0].type ==types.Message:
+    if request.json()['message'][0].type ==types.Message:
         message_handler(request)
 
-
-@dp.callback_query_handler(lambda c: c.data and c.data.startswith('btn'))
-async def process_callback_kb1btn1(callback_query: types.CallbackQuery):
-    js = new_collection.find({}, { '_id' : 0})
-    full = db_list(js)
-    code = callback_query.data[-1]
-    if  code.isdigit():
-        code = int(code)
-        full_text = ''
-        for i in range(len(full)):
-            if code == i:
-                for g in full[i]:
-                    full_text += g + '\n'
-                await bot.send_message(callback_query.from_user.id, full_text)
-
 @dp.message_handler(commands=['start'], state = '*')
-async def process_start_command(msg: types.Message):
+async def process_start_command(msg: types.Message, state: FSMContext):
     await bot.send_message(msg.from_user.id, 'Добрейший вечерочек!\nПиши /help, '
                         'чтобы узнать список доступных команд!')
 
 @dp.message_handler(commands=['help'], state = '*')
-async def process_help_command(msg: types.Message):
+async def process_help_command(msg: types.Message, state: FSMContext):
     mess = text(bold('Смотри, я могу ответить за следующее:'),
                 '/info - выведет список', '/worker - поиск по должности', 
                 '/edit - внесение изменений', sep = "\n")
     await bot.send_message(msg.from_user.id, mess, parse_mode=ParseMode.MARKDOWN)
 
 @dp.message_handler(commands=['edit'], state = '*')
-async def admin_command(msg: types.Message):
+async def admin_command(msg: types.Message, state: FSMContext):
+    #user_id = msg.from_user.id
+    #acsess = collection.find({}, {_id : 0})
+    #acces_id = db_list()
+    #for i in range(len(access_id)):
+    #    if acces_id[i][0] == str(user_id):
+    #        await state.set_state(States.ADMIN)
+    #        await bot.send_message(msg.from_user.id, "Что будем делать?", reply_markup=board_3)
+    #        return
+    #await bot.send_message(msg.from_user.id, "Ошибка доступа")
     await bot.send_message(msg.from_user.id, 'Введите пароль для перехода в режим администратора: ')  
     #проверка пароля
 
 @dp.message_handler(commands=['info'], state = '*')
-async def list_command(msg: types.Message):
+async def list_command(msg: types.Message, state: FSMContext):
     await bot.send_message(msg.from_user.id, "Как много информации тебе нужно?", reply_markup=board_1)
     #клав полная - фио
 
 @dp.message_handler(commands=['worker'], state = '*')
-async def process_worker_command(msg: types.Message):
-    board_2 = create_inline_keyboard()
-    await bot.send_message(msg.from_user.id, "Вот все руководство, выбирай", reply_markup=board_2)
+async def process_worker_command(msg: types.Message, state: FSMContext):
+    board_4 = create_reply_keyboard()
+    full_text = num_list()
+    await bot.send_message(msg.from_user.id, full_text)
+    await bot.send_message(msg.from_user.id, "Вот все руководство, выбирай", reply_markup=board_4)
     #выводит клав с должностями
 
 @dp.message_handler(state=States.ADMIN, content_types=['text'])
@@ -105,27 +92,13 @@ async def admin(msg: types.Message, state: FSMContext):
     elif text == 'Изменить':
         board_4 = create_reply_keyboard()
         await state.set_state(States.CHANGE)
-        js = adm_collection.find({}, {'doljname' : 1, '_id' : 0})
-        full = db_list(js)
-        g = 1
-        full_text = ''
-        for elem in full:
-            for i in elem:
-                full_text += f'{g}' + '. ' + i + '\n'
-                g += 1
+        full_text = num_list()
         await bot.send_message(msg.from_user.id, full_text)
         await bot.send_message(msg.from_user.id, "Это весь список, кого будем редактировать?", reply_markup=board_4)
     elif text == 'Удалить':
         board_4 = create_reply_keyboard()
         await state.set_state(States.DELETE)
-        js = adm_collection.find({}, {'doljname' : 1, '_id' : 0})
-        full = db_list(js)
-        g = 1
-        full_text = ''
-        for elem in full:
-            for i in elem:
-                full_text += f'{g}' + '. ' + i + '\n'
-                g += 1
+        full_text = num_list()
         await bot.send_message(msg.from_user.id, full_text)
         await bot.send_message(msg.from_user.id, "Это весь список, кого будем удалять?", reply_markup=board_4)
     elif text == 'Сохранить':
@@ -139,7 +112,16 @@ async def admin(msg: types.Message, state: FSMContext):
                 doc.pop('admin_id')
             full.append(doc)
         new_collection.insert_many(full)
+        state.finish()
         await bot.send_message(msg.from_user.id, "Все изменения сохранены", reply_markup=ReplyKeyboardRemove())
+
+@dp.message_handler(state=States.DOLJ, content_types=['text'])
+async def dolj(msg: types.Message, state: FSMContext):
+    dolj = msg.text
+    await state.set_state(States.ADMIN)
+    await state.update_data(doljname=dolj)
+    await state.set_state(States.FIO)
+    await bot.send_message(msg.from_user.id, 'Введи фамилию, имя и отчество через пробелы')
 
 @dp.message_handler(state=States.FIO, content_types=['text'])
 async def fio(msg: types.Message, state: FSMContext):
@@ -152,14 +134,6 @@ async def fio(msg: types.Message, state: FSMContext):
     await state.update_data(Fname=fio[0])
     await state.update_data(Name=fio[1])
     await state.update_data(Oname=fio[2])
-    await state.set_state(States.ADRESS)
-    await bot.send_message(msg.from_user.id, "Введи кабинет: ")
-
-@dp.message_handler(state=States.DOLJ, content_types=['text'])
-async def dolj(msg: types.Message, state: FSMContext):
-    dolj = msg.text
-    await state.set_state(States.ADMIN)
-    await state.update_data(doljname=dolj)
     await state.set_state(States.ADRESS)
     await bot.send_message(msg.from_user.id, "Введи кабинет: ")
 
@@ -216,7 +190,7 @@ async def change(msg: types.Message, state: FSMContext):
             for value in elem:
                 full_text += str(value) + '\n'
         await bot.send_message(msg.from_user.id, full_text)
-        await bot.send_message(msg.from_user.id, "Что будем менять?", reply_markup=board_5)
+        await bot.send_message(msg.from_user.id, "Что будем менять?", reply_markup=board_2)
     elif text == 'Назад':
         await state.set_state(States.ADMIN)
         await bot.send_message(msg.from_user.id, "Если это все, что ты хотел - жми 'Сохранить', "
@@ -244,8 +218,7 @@ async def change(msg: types.Message, state: FSMContext):
             await state.set_state(States.ADMIN)
             await bot.send_message(msg.from_user.id, "Если это все, что ты хотел - жми 'Сохранить', ну или выбирай, что будем делать", reply_markup=board_3)
         else:
-            await bot.send_message(msg.from_user.id, "Выбери, что будем менять на клавиатуре, либо напиши 'Назад', вернуться", reply_markup=board_5)
-
+            await bot.send_message(msg.from_user.id, "Выбери, что будем менять на клавиатуре, либо напиши 'Назад', вернуться", reply_markup=board_2)
 
 @dp.message_handler(state=States.DELETE, content_types=['text'])
 async def delete(msg: types.Message, state: FSMContext):
@@ -268,7 +241,6 @@ async def delete(msg: types.Message, state: FSMContext):
         await state.set_state(States.ADMIN)
         await bot.send_message(msg.from_user.id, "Если это все, что ты хотел - жми 'Сохранить', ну или выбирай, что будем делать", reply_markup=board_3)
 
-
 @dp.message_handler(content_types=['text'], state = '*')
 async def echo(msg: types.Message, state: FSMContext):
     text = msg.text
@@ -290,11 +262,19 @@ async def echo(msg: types.Message, state: FSMContext):
             full_text.insert(1, '-')
             full_text = ' '.join(full_text)
             await bot.send_message(msg.from_user.id, full_text)
+    elif text.isdigit():
+        js = new_collection.find({}, { '_id' : 0})
+        full = db_list(js)
+        code = int(text)
+        full_text = ''
+        for i in range(len(full)):
+            if code == i:
+                for g in full[i]:
+                    full_text += g + '\n'
+                await bot.send_message(msg.from_user.id, full_text)
     elif text == PASSWORD:
         await state.set_state(States.ADMIN)
-        check = await state.get_state()
-        print(check)
-        await bot.send_message(msg.from_user.id, "Теперь ты админ, что будем делать?", reply_markup=board_3)
+        await bot.send_message(msg.from_user.id, "Админь", reply_markup=board_3)
     else:
         await bot.send_message(msg.from_user.id, 'Я не знаю таких слов')
 
